612167d786a49958c149e477d41981408af16ed5
pierre <pierre.boisnard@live.fr>
2025-09-15 14:09:12 +0200

    add delete after 3 years for contactmessage

--- models/contactMessage.js
+++ models/contactMessage.js
@@ -6,7 +6,13 @@ const contactMessageSchema = new mongoose.Schema({
   phone: { type: String, required: true },
   message: { type: String, required: true },
   answered: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now }
+  
+  // ✅ MODIFICATION : Ajouter TTL sur createdAt
+  createdAt: { 
+    type: Date, 
+    default: Date.now,
+    expires: 60 * 60 * 24 * 365 * 3  // 94 608 000 secondes = 3 ans
+  }
 });
 
 module.exports = mongoose.model('ContactMessage', contactMessageSchema);
